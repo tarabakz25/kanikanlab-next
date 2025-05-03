41e6beef933b09251019a45729fb5ff6dd73da5b
Kz <aikikizuki@gmail.com>
2025-05-03 17:46:07 +0900

    ✨Update: コードの整形を行い、インポート文のスタイルを統一。SVGファイルにコメントを追加し、Tailwind CSSの設定を改善。全体の可読性を向上させた。

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-        animation: {
-            'text-underline': 'text-underline position-absolute left-0 bottom-2 w-full h-1 bg-black transition-all duration-300 ease-in-out',
+      animation: {
+        "text-underline":
+          "text-underline position-absolute left-0 bottom-2 w-full h-1 bg-black transition-all duration-300 ease-in-out",
+      },
+      keyframes: {
+        "text-underline": {
+          "0%": {
+            transform: "scaleX(0)",
+          },
+          "100%": {
+            transform: "scaleX(1)",
+          },
         },
-        keyframes: {
-            'text-underline': {
-                '0%': {
-                    transform: 'scaleX(0)',
-                },
-                '100%': {
-                    transform: 'scaleX(1)',
-                },
-            },
-        },
-        fontFamily: {
-            sans: ['krok', 'source-han-sans-cjk-ja', 'fot-tsukuardgothic-std', 'sans-serif'],
-            mono: ['anonymous-pro', 'monospace'],
-        },
-    }
+      },
+      fontFamily: {
+        sans: [
+          "krok",
+          "source-han-sans-cjk-ja",
+          "fot-tsukuardgothic-std",
+          "sans-serif",
+        ],
+        mono: ["anonymous-pro", "monospace"],
+      },
+    },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
 
-export default config
+export default config;
